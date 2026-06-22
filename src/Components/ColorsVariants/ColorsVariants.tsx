377be777aa804eb2c12dd445dfd7f0f63f3003fd
Illya Sukhomlyn <gollpfd@gmail.com>
2026-06-22 17:40:33 +0300

    update

---
+++ src/Components/ColorsVariants/ColorsVariants.tsx
@@ -0,0 +1,20 @@
+export function ColorsVariants() {
+  return (
+    <section className="ColorsVariants">
+      <div className="container">
+        <h3 className="ColorsVariants-subTitle">ВАРІАНТИ КОЛЬОРІВ</h3>
+        <h2 className="ColorsVariants-title">
+          Оберіть ідеальне поєднання для вашого інтер’єру
+        </h2>
+        <div className="line"></div>
+
+        <div className="ColorsVariants-cards">
+          <div className="ColorsVariants-card">
+            <div className="ColorsVariants-card-color"></div>
+            <p className="ColorsVariants-card-title"></p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
